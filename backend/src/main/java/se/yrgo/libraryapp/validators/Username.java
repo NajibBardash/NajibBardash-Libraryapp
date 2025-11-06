3afaf6c5c6e98a79e8ebc095713e769455492f1d
Najib Bardash <najib.bardash@gmail.com>
2025-11-06 15:59:07 +0100

    Added check to username-validation so that a username mustn't only include special characters

--- backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
+++ backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
@@ -5,10 +5,11 @@ import java.util.regex.Pattern;
 /**
  * This validator checks that the username match our high standard for proper names.
  * 
- * I.e. no funny characters or whitespace and at least four and no more than 30 characters long.
+ * I.e. no funny characters or only special characters, or whitespace and at least four and no more than 30 characters long.
  */
 public final class Username {
-    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-9]{4,30}");
+    private static final Pattern regex = Pattern.compile("[@._a-zA-Z0-9]{4,30}");
+    private static final Pattern onlySpecialCharactersRegex = Pattern.compile("[@._]{4,30}");
 
     private Username() {}
 
@@ -18,12 +19,13 @@ public final class Username {
      * A username should be at least four and no more than 30 characters long and only
      * contain ASCII letters, numbers and the characters @, ., _ and -.
      * It should not contain any other letters, not even whitespace.
+     * It should not only contain sppecial characters.
      * 
      * @param name the name to check
      * @return true if valid, false if not
      * 
      */
     public static boolean validate(String name) {
-        return regex.matcher(name).matches();
+        return regex.matcher(name).matches() && !onlySpecialCharactersRegex.matcher(name).matches();
     }
 }
