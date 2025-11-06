f07ed38b8918e70612a737d7e69b57adfe8f92fb
Najib Bardash <najib.bardash@gmail.com>
2025-11-06 14:45:49 +0100

    Added maxlength to username of 30 characters

--- backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
+++ backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
@@ -5,17 +5,17 @@ import java.util.regex.Pattern;
 /**
  * This validator checks that the username match our high standard for proper names.
  * 
- * I.e. no funny characters or whitespace and at least four characters long.
+ * I.e. no funny characters or whitespace and at least four and no more than 30 characters long.
  */
 public final class Username {
-    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-9]{4,}");
+    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-9]{4,30}");
 
     private Username() {}
 
     /**
      * Validates if the given name is a valid username.
      * 
-     * A username should be at least four characters long and only
+     * A username should be at least four and no more than 30 characters long and only
      * contain ASCII letters, numbers and the characters @, ., _ and -.
      * It should not contain any other letters, not even whitespace.
      * 
