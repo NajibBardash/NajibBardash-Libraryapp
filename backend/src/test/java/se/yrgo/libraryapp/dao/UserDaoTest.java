e1d5831ffbd362ca02806986693034a2d6cf9f83
Najib Bardash <najib.bardash@gmail.com>
2025-11-21 08:48:35 +0100

    Added mockito and UserDaoTest

---
+++ backend/src/test/java/se/yrgo/libraryapp/dao/UserDaoTest.java
@@ -0,0 +1,65 @@
+package se.yrgo.libraryapp.dao;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import se.yrgo.libraryapp.entities.*;
+
+import javax.sql.DataSource;
+
+import java.sql.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+public class UserDaoTest {
+    @Mock
+    private DataSource ds;
+
+    @Mock
+    private Connection conn;
+
+    @Mock
+    private Statement stmt;
+
+    @Mock
+    private ResultSet rs;
+
+    @Test
+    void getExistingLoginInfo() throws SQLException {
+        final String username = "test";
+        final UserId id = UserId.of(1);
+        final String passwordHash =
+                "$argon2i$v=19$m=16,t=2,p=1$QldXU09Sc2dzOWdUalBKQw$LgKb6x4usOpDLTlXCBVhxA";
+
+        when(ds.getConnection()).thenReturn(conn);
+        when(conn.createStatement()).thenReturn(stmt);
+        when(stmt.executeQuery(contains(username))).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getInt("id")).thenReturn(id.getId());
+        when(rs.getString("password_hash")).thenReturn(passwordHash);
+
+        UserDao userDao = new UserDao(ds);
+        LoginInfo info = userDao.getLoginInfo(username).get();
+        assertThat(info.getUserId()).isEqualTo(id);
+        assertThat(info.getPasswordHash()).isEqualTo(passwordHash);
+    }
+
+    @Test
+    void getNonExistingLoginInfo() throws SQLException {
+        final String username = "test";
+
+        when(ds.getConnection()).thenReturn(conn);
+        when(conn.createStatement()).thenReturn(stmt);
+        when(stmt.executeQuery(contains(username))).thenReturn(rs);
+        when(rs.next()).thenReturn(false);
+
+        UserDao userDao = new UserDao(ds);
+        assertThat(userDao.getLoginInfo(username)).isEmpty();
+    }
+}
